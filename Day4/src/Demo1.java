a3a5e7502d2c42c4eefa2b18c54d18a1acb770eb
waseem <waseem@DESKTOP-VVUKLKF>
2022-06-03 16:07:44 +0530

    first commit

---
+++ Day4/src/Demo1.java
@@ -0,0 +1,15 @@
+
+public class Demo1 {
+	float x;
+
+	public static void main(String[] args) {
+		Demo1 myObj1 = new Demo1();
+		Demo1 myObj2 = new Demo1();
+		myObj1.x = 24;
+		myObj2.x= 55;
+		System.out.println(myObj1.x);
+		System.out.println(myObj2.x);
+
+	}
+
+}
---
+++ Day4/src/Demo2.java
@@ -0,0 +1,15 @@
+
+public class Demo2 {
+	double x;
+
+	public static void main(String[] args) {
+		Demo2 myObj1 = new Demo2();
+		Demo2 myObj2 = new Demo2();
+		myObj1.x = 24;
+		myObj2.x= 55;
+		System.out.println(myObj1.x);
+		System.out.println(myObj2.x);
+
+	}
+
+}
